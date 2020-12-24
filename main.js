206de7f2bd3c1a863b09c056623e67102be5d363
felipeoli7eira <me.felipeoliveira@gmail.com>
2020-12-23 22:35:46 -0300

    with view engine

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
-const htmlPDF = require('html-pdf');
+const htmlPDF = require('html-pdf')
+const ejs = require('ejs')
 
-const content = `
+ejs.renderFile('./pdf.ejs', {}, (error, htmlFile) => {
 
-    <div style="text-align: center">
-
-        <h1 style="font-weight: light">HTML PDF - NODE JS</h1>
-
-        <p class="color: #333">Lorem ipsum dolor sit amet culpa eu</p>
-
-        <nav>
-            <a href="https://www.npmjs.com/package/html-pdf">read more</a>
-        </nav>
-
-    </div>
-`
-
-htmlPDF.create(content, {}).toFile('./example.pdf', (error, response) => {
     if(!error)
     {
-        console.log(response)
-        return true
+        htmlPDF.create(htmlFile, {}).toFile('./example.pdf', (error, response) => {
+            if(!error)
+            {
+                console.log(response)
+                return true
+            }
+        
+            console.log('error: ' + error)
+            return false
+        })
     }
 
-    console.log('error: ' + error)
+    return false
 })
